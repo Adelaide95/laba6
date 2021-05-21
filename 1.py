43d2a331684dd31c21006bc8b5e1446adee0b145
Adelaide95 <Adelaide2002@mail.ru>
2021-05-21 09:02:09 +0300

    new

--- 1.py
+++ 1.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import sys
 if __name__ == '__main__':
-    import sys
-a = list(map(int, input().split()))
-if len(a) != 10:
-    print("Неверный размер списка", file=sys.stderr)
-    exit(1)
-a_fil = []
-for elem in a:
-    if elem > 8 and elem < 18 and elem % 10 == 0:
+    a = list(map(int, input().split()))
+    if len(a) != 10:
+        print("Неверный размер списка", file=sys.stderr)
+        exit(1)
+    a_fil = []
+    for elem in a:
+        if elem > 8 and elem < 18 and elem % 10 == 0:
 
-        a_fil.append(elem)
+            a_fil.append(elem)
 
-if len(a_fil) == 0:
-    print(0)
-else:
-    res = 1
-    for elem in a_fil:
-        res *= elem
-print(res)
+    if len(a_fil) == 0:
+        print(0)
+    else:
+        res = 1
+        for elem in a_fil:
+            res *= elem
+    print(res)
